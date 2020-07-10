2168ea6f1eaf7e57869be8680f7084ec54d97a6a
HexClanSB <68089330+HexClanSB@users.noreply.github.com>
2020-07-09 20:04:44 -0500

    Add files via upload

---
+++ 1x-selfbot-master/install.bat
@@ -0,0 +1,6 @@
+@echo off
+cls
+title 1x Modules Installer
+py -3.8 -m pip install -r requirements.txt
+echo Finished!
+pause
---
+++ 1x-selfbot-master/launch.bat
@@ -0,0 +1,6 @@
+@echo off
+cls
+title 1x Launcher
+py -3.8 1x.py
+echo SHIT IDK WHY NOT WORKING
+pause
